09d604f30ff109acdfee00a9734f71b5cd137ad6
Chandrakala Chippa <chandra.chippa@gmail.com>
2025-09-07 23:00:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
 
+MODEL_NAME = "chandra1024/bart_cnn"
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_NAME)
+
+translator = pipeline("translation", model="Helsinki-NLP/opus-mt-en-hi")
